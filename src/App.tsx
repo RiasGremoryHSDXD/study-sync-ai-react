a8044a759fd86d6c38d1afe6b15782ab972b076f
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-06-18 18:33:33 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LogInForm from './pages/LogInForm/index'
+
+function App() {
+
+  return (
+    <div className='bg-amber-500'>
+      <LogInForm/>
+    </div>
+  )
+}
+
+export default App
